b180db5cefa3ec469d39130d062df6a09f775753
Ahromanin <ahromanin@gmail.com>
2022-09-19 23:07:22 -0300

    funcion validar telefono y mail

--- Carta Resto-Bar/main.js
+++ Carta Resto-Bar/main.js
@@ -1,15 +1,43 @@
+function enviar(emailId, telefono)
+{
+if( ValidarNroTel(telefono) && validateEmail(emailId))
+{
+alert("Formulario enviado");
+}
+else
+{
+alert("Formulario no enviado");
+}
+}
+
+
 function validateEmail(emailId)
 {
 var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 if(emailId.value.match(mailformat))
 {
-document.form1.text1.focus();
+document.form1.email.focus();
 return true;
 }
 else
 {
 alert("Mail ingresado no es valido");
-document.form1.text1.focus();
+document.form1.email.focus();
 return false;
 }
 } 
+
+
+function ValidarNroTel(telefono)
+{
+ var nrotel = /^\d{10}$/;
+ if((telefono.value.match(nrotel)))
+ {
+ return true;
+ }
+ else
+ {
+ alert("teléfono inválido (Maximo 10 digitos)");
+ return false;
+ }
+}
